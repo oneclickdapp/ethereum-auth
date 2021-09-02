2e162bb163eae8d386b6ac2da724492583f7a1bf
patrick <blockchainbuddha@gmail.com>
2021-09-02 10:40:01 -0400

    working

--- webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "eval" // Fastest for development
-  // devtool: "eval-source-map" // NOTE: Useful for debugging
-});
