afc8bb5d52f6b699f750a984537a02286e01e5d3
patrick <blockchainbuddha@gmail.com>
2021-09-02 18:39:28 -0400

    update demo

--- examples/eth-auth-demo/web/src/pages/LoginPage/LoginPage.js
+++ examples/eth-auth-demo/web/src/pages/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import { Link, routes, navigate } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 import { useParams } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { MobileWalletIcon, MetamaskIcon } from 'src/components/Icons'
 
 const LoginPage = () => {
   const { logIn } = useAuth()
@@ -27,22 +28,30 @@ const LoginPage = () => {
       <h1 className="text-xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl">
         Login
       </h1>
-      <button
-        className={
-          'mt-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700'
-        }
-        onClick={() => onLogin()}
-      >
-        Log in with Ethereum Browser
-      </button>
-      <button
-        className={
-          'mt-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700'
-        }
-        onClick={() => onLogin('walletConnect')}
-      >
-        Log in with Wallet Connect
-      </button>
+      <ul>
+        <li>
+          <button
+            className={
+              'mt-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border rounded-md shadow-sm text-base font-medium hover:bg-gray-300'
+            }
+            onClick={() => onLogin()}
+          >
+            <MetamaskIcon />
+            Log in with Ethereum
+          </button>
+        </li>
+        <li>
+          <button
+            className={
+              'mt-4 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border rounded-md shadow-sm text-base font-medium hover:bg-gray-300'
+            }
+            onClick={() => onLogin('walletConnect')}
+          >
+            <MobileWalletIcon />
+            Log in with Wallet Connect
+          </button>
+        </li>
+      </ul>
     </>
   )
 }
