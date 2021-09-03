cd09b1ccac8a3bcd918d81122bdf51e96fd41b30
patrick <blockchainbuddha@gmail.com>
2021-09-03 19:23:06 -0400

    update migrations for hosted postgres db

--- examples/eth-auth-demo/api/db/migrations/20210903232243_init/migration.sql
+++ examples/eth-auth-demo/api/db/migrations/20210903232243_init/migration.sql
@@ -1,16 +1,19 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "authDetailId" TEXT NOT NULL,
-    FOREIGN KEY ("authDetailId") REFERENCES "AuthDetail" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "AuthDetail" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "nonce" TEXT NOT NULL,
-    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -18,3 +21,6 @@ CREATE UNIQUE INDEX "User.address_unique" ON "User"("address");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_authDetailId_unique" ON "User"("authDetailId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD FOREIGN KEY ("authDetailId") REFERENCES "AuthDetail"("id") ON DELETE CASCADE ON UPDATE CASCADE;
