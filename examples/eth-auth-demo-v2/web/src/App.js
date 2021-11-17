1171a1e80cb4792682ed489c5c60d5b27fdde78b
patrick <blockchainbuddha@gmail.com>
2021-11-16 21:45:00 -0500

    App.js

--- examples/eth-auth-demo-v2/web/src/App.js
+++ examples/eth-auth-demo-v2/web/src/App.js
@@ -1,6 +1,5 @@
 import { AuthProvider } from '@redwoodjs/auth'
 
-
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -10,54 +9,14 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
-let ethereum
-
-const ApolloInjector = ({ children }) => {
-  const { uri, headers } = useFetchConfig()
-  try {
-    const graphQLClient = new ApolloClient({
-      cache: new InMemoryCache(),
-      uri,
-      headers,
-    })
-    // Default option using Apollo Client
-    const makeRequest = (mutation, variables) =>
-      graphQLClient.mutate({
-        mutation,
-        variables,
-      })
-
-    // Alternative option using graphql-hooks
-    // You'll also need to modify graphQLClient
-    // const makeRequest = (query, variables) =>
-    //   graphQLClient.request({
-    //     query,
-    //     variables,
-    //   })
-
-    ethereum = new EthereumAuthClient({
-      makeRequest,
-      infuraId: process.env.INFURA_ID,
-      debug: process.NODE_ENV === 'development',
-    })
-  } catch (e) {
-    console.log(e)
-  }
-  return React.cloneElement(children, { client: ethereum })
-}
-
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <FetchConfigProvider>
-        <ApolloInjector>
-          <AuthProvider type="dbAuth">
-            <RedwoodApolloProvider>
-              <Routes />
-            </RedwoodApolloProvider>
-          </AuthProvider>
-        </ApolloInjector>
-      </FetchConfigProvider>
+      <AuthProvider type="ethereum">
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
